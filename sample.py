f794624649f0da3987f1a29ce9d92a7bf9b6174e
JanZaplatil <30730161+JanZaplatil@users.noreply.github.com>
2020-11-04 12:08:27 +0100

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,27 +1,3 @@
-'''def najvecjeStevilo(aList):
-  sestevek =  0
-  for a in aList:
-    sestevek = sestevek + a
-  print(sestevek)
-
-
-elementi = [1,2,4,5,]
-
-najvecjeStevilo(elementi)
-
-
-
-stevila = []
-for c in range(7):
-  c = int(input("vsesi st: "))
-  stevila.append(c)
-print(stevila)
-
-najvecjeStevilo(stevila)'''
-
-
-
-
 
 def izpis(aList):
   naj = 0
@@ -50,9 +26,3 @@ print(stevila)
 
 izpis(stevila)
 
-
-'''aList = [5,6,7,8,9]
-
-for d in range(len(aList)):
-  print(d)
-  print(aList[d])'''
